20f682e7ec115d4f3fe4034fd7bceb1600570c8a
Aleksei Arro <polsck@gmail.com>
2024-10-06 13:07:38 +0400

    chore: add tutorial-react-tictactoe-game project

---
+++ react/_outdated/tutorial-react-tictactoe-game/src/index.js
@@ -0,0 +1,14 @@
+/** TODO:
+ * Tasklist
+ * ========
+ * Rework with hooks
+ * Display the location for each move in the format (col, row) in the move history list.
+ * Add a toggle button that lets you sort the moves in either ascending or descending order.
+ * When no one wins, display a message about the result being a draw.
+ */
+
+import './common.scss';
+
+import initApp from './init-app.jsx';
+
+initApp();
---
+++ react/_outdated/tutorial-react-tictactoe-game/src/utils/calculate-winner.js
@@ -0,0 +1,23 @@
+const winnerLines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+export default (squares) => {
+  for (let i = 0; i < winnerLines.length; i++) {
+    const [a, b, c] = winnerLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return {
+        symbol: squares[a],
+        line: winnerLines[i],
+      };
+    }
+  }
+  return null;
+}
