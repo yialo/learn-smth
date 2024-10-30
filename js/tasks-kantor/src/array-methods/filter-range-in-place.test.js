fdb1e04f638f2f02f99e6152441bc7699db89ad4
Aleksei Arro <polsck@gmail.com>
2024-10-30 22:19:28 +0400

    refactor: filterRangeInPlace test

--- js/tasks-kantor/src/array-methods/filter-range-in-place.test.js
+++ js/tasks-kantor/src/array-methods/filter-range-in-place.test.js
@@ -2,26 +2,15 @@ import { expect, describe, test } from 'vitest';
 
 import { filterRangeInPlace } from './filter-range-in-place';
 
-test('viaCounter', () => {
+test.for([
+  ['viaCounter', filterRangeInPlace.viaCounter],
+  ['viaWhile', filterRangeInPlace.viaWhile],
+  ['viaForLoop', filterRangeInPlace.viaForLoop],
+])('%s', ([, fn]) => {
   const inputArray = [5, 3, 8, 1];
-  const result = filterRangeInPlace.viaCounter(inputArray, 1, 4);
+  const result = fn(inputArray, 1, 4);
   const expected = [3, 1];
-  expect(result).toEqual(expected);
-  expect(inputArray).toBe(result);
-});
 
-test('viaWhile', () => {
-  const inputArray = [5, 3, 8, 1];
-  const result = filterRangeInPlace.viaWhile(inputArray, 1, 4);
-  const expected = [3, 1];
-  expect(result).toEqual(expected);
-  expect(inputArray).toBe(result);
-});
-
-test('viaForLoop', () => {
-  const inputArray = [5, 3, 8, 1];
-  const result = filterRangeInPlace.viaForLoop(inputArray, 1, 4);
-  const expected = [3, 1];
   expect(result).toEqual(expected);
   expect(inputArray).toBe(result);
 });
