e61f990c5e92f09e1c1ec91216003b7abc276faf
Aleksei Arro <polsck@gmail.com>
2024-11-04 23:27:52 +0400

    refactor: move permissionsApiClient to app scope

--- fsd/paromov-2024/lesson-2/di-react-context/src/app/ui.tsx
+++ fsd/paromov-2024/lesson-2/di-react-context/src/app/ui.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { PermissionsProvider } from '../permissions';
 import { SessionProvider, useUserId } from '../session';
 import { TaskList, TasksProvider } from '../tasks';
@@ -13,9 +14,11 @@ const TasksPermissionsProvider: React.FC<React.PropsWithChildren> = ({
   );
 };
 
-const permissionsApiClient = createPermissionsApiClient();
-
 export const App: React.FC = () => {
+  const [permissionsApiClient] = React.useState(() =>
+    createPermissionsApiClient(),
+  );
+
   return (
     <PermissionsProvider apiClient={permissionsApiClient}>
       <SessionProvider>
