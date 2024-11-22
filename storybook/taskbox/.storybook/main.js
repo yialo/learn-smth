079f4dce7dd7d855ac27191cb89f72e7a169a3aa
Aleksei Arro <polsck@gmail.com>
2024-11-22 19:39:36 +0400

    chore: bump storybook package version

--- storybook/taskbox/.storybook/main.js
+++ storybook/taskbox/.storybook/main.js
@@ -1,15 +1,18 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
+  },
+  core: {
+    disableTelemetry: true,
   },
 };
 export default config;
