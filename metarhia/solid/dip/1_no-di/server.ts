9ed7e62a4e03ad6c24d388f2e208a77af7006380
Aleksei Arro <polsck@gmail.com>
2024-12-09 12:37:46 +0400

    feat: add Storage module

--- metarhia/solid/dip/1_no-di/server.ts
+++ metarhia/solid/dip/1_no-di/server.ts
@@ -1,5 +1,21 @@
 import { createServer } from 'node:http';
 import { logger } from './logger';
-import { Storage } from './storage';
+import { AppStorage } from './storage';
 
-export declare function serveStatic(folder: string, port: number): void;
+export type ServeStaticFn = (folder: string, port: number) => void;
+
+const MIME_TYPES = {
+  default: 'application/octet-stream',
+  html: 'text/html; charset=UTF-8',
+  js: 'application/javascript; charset=UTF-8',
+  json: 'application/json',
+  css: 'text/css',
+  png: 'image/png',
+  jpg: 'image/jpg',
+  gif: 'image/gif',
+  ico: 'image/x-icon',
+  svg: 'image/svg+xml',
+  txt: 'text/plain',
+} as const;
+
+export const serveStatic: ServeStaticFn = (folder, port) => {};
--- metarhia/solid/dip/1_no-di/storage.ts
+++ metarhia/solid/dip/1_no-di/storage.ts
@@ -1,13 +1,49 @@
-export declare class PreparedFile {
+import fs from 'node:fs';
+import path from 'node:path';
+
+interface IPreparedFile {
   found: boolean;
   ext: string;
   stream: NodeJS.ReadableStream;
-
-  constructor(found: boolean, ext: string, stream: NodeJS.ReadableStream);
 }
 
-export declare class Storage {
-  constructor(folder: string);
+class PreparedFileImpl implements IPreparedFile {
+  constructor(
+    public found: boolean,
+    public ext: string,
+    public stream: NodeJS.ReadableStream,
+  ) {}
+}
 
-  prepare(filename: string): Promise<PreparedFile>;
+interface IAppStorage {
+  prepare(filename: string): Promise<IPreparedFile>;
+}
+
+export class AppStorage implements IAppStorage {
+  #folder = '';
+
+  constructor(folder: string) {
+    this.#folder = folder;
+  }
+
+  async prepare(filename: string): Promise<IPreparedFile> {
+    const paths = [this.#folder, filename];
+
+    if (filename.endsWith('/')) {
+      paths.push('index.html');
+    }
+
+    const filePath = path.join(...paths);
+    const pathTraversal = !filePath.startsWith(this.#folder);
+    const exists = await fs.promises.access(filePath).then(
+      () => true,
+      () => false,
+    );
+    const found = !pathTraversal && exists;
+    const streamPath = found ? filePath : this.#folder + '/404.html';
+    const ext = path.extname(streamPath).substring(1).toLowerCase();
+    const stream = fs.createReadStream(streamPath);
+
+    return new PreparedFileImpl(found, ext, stream);
+  }
 }
