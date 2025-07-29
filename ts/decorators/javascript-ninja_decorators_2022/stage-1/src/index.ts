9d64f33406d727d2ed814c42a6acef7c85f13262
Aleksei Arro <polsck@gmail.com>
2025-07-29 20:39:08 +0400

    study: draft javascript-ninja_decorators_2022

---
+++ ts/decorators/javascript-ninja_decorators_2022/stage-1/src/index.ts
@@ -0,0 +1,21 @@
+function createDecorator(place: string) {
+  return function decorator(...args: any[]) {
+    console.log(`[decorator call] (${place}):`, args);
+  };
+}
+
+@createDecorator('class')
+class MyClass {
+  set a(value: any) {}
+
+  @createDecorator('property')
+  p = 1;
+
+  @createDecorator('method')
+  m() {}
+
+  @createDecorator('getter')
+  get a() {
+    return 'a value';
+  }
+}
