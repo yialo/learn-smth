955faa1ae8180a352215f9861032ad4578566f06
Aleksei Arro <polsck@gmail.com>
2025-09-07 12:55:12 +0400

    refactor: add NoSuchElementException

--- clean-code/chapter-14_cli-args-parsing/src/args/arg-list-iterator.test.ts
+++ clean-code/chapter-14_cli-args-parsing/src/args/arg-list-iterator.test.ts
@@ -1,47 +1,53 @@
-import { expect, describe, test } from 'vitest';
+import { expect, test } from 'vitest';
 
-import { ArgListIterator } from './arg-list-iterator.ts';
+import {
+  ArgListIterator,
+  NoSuchElementException,
+} from './arg-list-iterator.ts';
 
-describe('ArgListTracker', () => {
-  test('empty list', () => {
-    const argList: string[] = [];
-    const iterator = new ArgListIterator(argList);
+test('ArgListIterator', () => {
+  const argList = ['a', 'b', 'c'];
+  const iterator = new ArgListIterator(argList);
 
-    expect(iterator.current()).toBeUndefined();
+  expect(iterator.hasPrevious()).toBe(false);
+  expect(iterator.previousIndex()).toBe(-1);
+  assertPreviousElementException(iterator);
 
-    expect(iterator.hasPrevious()).toBe(false);
-    expect(iterator.previousIndex()).toBe(-1);
-    expect(iterator.previous()).toBe(iterator.current());
+  expect(iterator.hasNext()).toBe(true);
+  expect(iterator.nextIndex()).toBe(0);
 
-    expect(iterator.hasNext()).toBe(false);
-    expect(iterator.nextIndex()).toBe(-1);
-    expect(iterator.next()).toBe(iterator.current());
-  });
+  expect(iterator.next()).toBe('a');
+  expect(iterator.next()).toBe('b');
+  expect(iterator.next()).toBe('c');
+  expect(iterator.hasNext()).toBe(false);
+  expect(iterator.nextIndex()).toBe(-1);
+  assertNextElementException(iterator);
 
-  test('non-empty list', () => {
-    const argList = ['a', 'b', 'c'];
-    const iterator = new ArgListIterator(argList);
-
-    expect(iterator.current()).toBeUndefined();
-
-    expect(iterator.hasPrevious()).toBe(false);
-    expect(iterator.previousIndex()).toBe(-1);
-    expect(iterator.previous()).toBe(iterator.current());
-
-    expect(iterator.hasNext()).toBe(true);
-    expect(iterator.nextIndex()).toBe(0);
-
-    expect(iterator.next()).toBe('a');
-    expect(iterator.next()).toBe('b');
-    expect(iterator.next()).toBe('c');
-    expect(iterator.hasNext()).toBe(false);
-    expect(iterator.nextIndex()).toBe(iterator.currentIndex());
-    expect(iterator.next()).toBe(iterator.current());
-
-    expect(iterator.previous()).toBe('b');
-    expect(iterator.previous()).toBe('a');
-    expect(iterator.hasPrevious()).toBe(false);
-    expect(iterator.previousIndex()).toBe(0);
-    expect(iterator.previous()).toBe(iterator.current());
-  });
+  expect(iterator.previous()).toBe('b');
+  expect(iterator.previous()).toBe('a');
+  expect(iterator.hasPrevious()).toBe(false);
+  expect(iterator.previousIndex()).toBe(-1);
+  assertPreviousElementException(iterator);
 });
+
+const assertPreviousElementException = (iterator: ArgListIterator) => {
+  try {
+    iterator.previous();
+  } catch (error) {
+    expect(error).toBeInstanceOf(NoSuchElementException);
+    if (error instanceof NoSuchElementException) {
+      expect(error.message).toMatch('previous');
+    }
+  }
+};
+
+const assertNextElementException = (iterator: ArgListIterator) => {
+  try {
+    iterator.next();
+  } catch (error) {
+    expect(error).toBeInstanceOf(NoSuchElementException);
+    if (error instanceof NoSuchElementException) {
+      expect(error.message).toMatch('next');
+    }
+  }
+};
--- clean-code/chapter-14_cli-args-parsing/src/args/arg-list-iterator.ts
+++ clean-code/chapter-14_cli-args-parsing/src/args/arg-list-iterator.ts
@@ -7,37 +7,45 @@ export class ArgListIterator {
     this.#currentIndex = -1;
   }
 
-  current(): string | undefined {
+  #current(): string | undefined {
     return this.#list[this.#currentIndex];
-  }
-
-  currentIndex(): number {
-    return this.#currentIndex;
   }
 
   hasNext(): boolean {
     return this.#currentIndex < this.#list.length - 1;
   }
 
-  next(): string | undefined {
-    if (this.hasNext()) this.#currentIndex++;
-    return this.current();
+  next(): string {
+    if (!this.hasNext()) {
+      throw new NoSuchElementException('next');
+    }
+    this.#currentIndex++;
+    return this.#current() ?? '';
   }
 
   nextIndex(): number {
-    return this.hasNext() ? this.#currentIndex + 1 : this.#currentIndex;
+    return this.hasNext() ? this.#currentIndex + 1 : -1;
   }
 
   hasPrevious(): boolean {
     return this.#currentIndex > 0;
   }
 
-  previous(): string | undefined {
-    if (this.hasPrevious()) this.#currentIndex--;
-    return this.current();
+  previous(): string {
+    if (!this.hasPrevious()) {
+      throw new NoSuchElementException('previous');
+    }
+    this.#currentIndex--;
+    return this.#current() ?? '';
   }
 
   previousIndex(): number {
-    return this.hasPrevious() ? this.#currentIndex - 1 : this.#currentIndex;
+    return this.hasPrevious() ? this.#currentIndex - 1 : -1;
+  }
+}
+
+export class NoSuchElementException extends Error {
+  constructor(method: 'next' | 'previous') {
+    super(`${ArgListIterator.name} has no ${method} element.`);
   }
 }
