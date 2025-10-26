1a8593b6cd3c85ba3ab748baed4f74904489a7e8
Aleksei Arro <polsck@gmail.com>
2025-10-26 21:17:07 +0400

    study(tt_zod-tutorial): complete lesson 2

--- zod/total-typescript_zod-tutorial/src/02-object.problem.ts
+++ zod/total-typescript_zod-tutorial/src/02-object.problem.ts
@@ -3,8 +3,9 @@
 import { expect, it } from "vitest";
 import { z } from "zod";
 
-const PersonResult = z.unknown();
-//                   ^ 🕵️‍♂️
+const PersonResult = z.object({
+  name: z.string(),
+});
 
 export const fetchStarWarsPersonName = async (id: string) => {
   const data = await fetch(
