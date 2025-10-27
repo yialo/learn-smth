ebfe11fcb858fffeb6b699b7489c416d5fc2d550
Aleksei Arro <polsck@gmail.com>
2025-10-27 20:42:33 +0400

    study(tt_zod-tutorial): complete lesson 9

--- zod/total-typescript_zod-tutorial/src/09-composing-objects.problem.ts
+++ zod/total-typescript_zod-tutorial/src/09-composing-objects.problem.ts
@@ -1,24 +1,28 @@
-import { z } from "zod";
-import { Equal, Expect } from "./helpers/type-utils";
+import { z } from 'zod';
+import { Equal, Expect } from './helpers/type-utils';
 
 /**
  * 🕵️‍♂️ Refactor this code below to reduce the duplication,
  * while also making sure the cases don't go red!
  */
 
-const User = z.object({
+const WithId = z.object({
   id: z.string().uuid(),
+});
+
+const User = z.object({
+  ...WithId.shape,
   name: z.string(),
 });
 
 const Post = z.object({
-  id: z.string().uuid(),
+  ...WithId.shape,
   title: z.string(),
   body: z.string(),
 });
 
 const Comment = z.object({
-  id: z.string().uuid(),
+  ...WithId.shape,
   text: z.string(),
 });
 
