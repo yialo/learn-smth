2df86165abd79aff0bd891cf6a24c8b020c3b32c
Aleksei Arro <polsck@gmail.com>
2025-10-27 22:47:07 +0400

    study(tt_zod-tutorial): complete lesson 10

--- zod/total-typescript_zod-tutorial/src/10-transform.problem.ts
+++ zod/total-typescript_zod-tutorial/src/10-transform.problem.ts
@@ -1,12 +1,14 @@
 // CODE
 
-import { expect, it } from "vitest";
-import { z } from "zod";
+import { expect, it } from 'vitest';
+import { z } from 'zod';
 
-const StarWarsPerson = z.object({
-  name: z.string(),
-});
-//^ 🕵️‍♂️
+const StarWarsPerson = z
+  .object({ name: z.string() })
+  .transform<{ name: string; nameAsArray: string[] }>((person) => ({
+    ...person,
+    nameAsArray: person.name.split(' '),
+  }));
 
 const StarWarsPeopleResults = z.object({
   results: z.array(StarWarsPerson),
@@ -14,7 +16,7 @@ const StarWarsPeopleResults = z.object({
 
 export const fetchStarWarsPeople = async () => {
   const data = await fetch(
-    "https://www.totaltypescript.com/swapi/people.json",
+    'https://www.totaltypescript.com/swapi/people.json',
   ).then((res) => res.json());
 
   const parsedData = StarWarsPeopleResults.parse(data);
@@ -24,9 +26,9 @@ export const fetchStarWarsPeople = async () => {
 
 // TESTS
 
-it("Should resolve the name and nameAsArray", async () => {
+it('Should resolve the name and nameAsArray', async () => {
   expect((await fetchStarWarsPeople())[0]).toEqual({
-    name: "Luke Skywalker",
-    nameAsArray: ["Luke", "Skywalker"],
+    name: 'Luke Skywalker',
+    nameAsArray: ['Luke', 'Skywalker'],
   });
 });
