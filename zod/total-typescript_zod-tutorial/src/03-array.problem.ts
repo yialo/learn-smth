b655af4d71876bef0ebb735557084106b7af53fc
Aleksei Arro <polsck@gmail.com>
2025-10-27 14:52:59 +0400

    study(tt_zod-tutorial): complete lesson 3

--- zod/total-typescript_zod-tutorial/src/03-array.problem.ts
+++ zod/total-typescript_zod-tutorial/src/03-array.problem.ts
@@ -1,29 +1,30 @@
 // CODE
 
-import { expect, it } from "vitest";
-import { z } from "zod";
+import { expect, it } from 'vitest';
+import { z } from 'zod';
 
 const StarWarsPerson = z.object({
   name: z.string(),
 });
-
-const StarWarsPeopleResults = z.unknown();
-//                            ^ 🕵️‍♂️
+const StarWarsPeopleResults = z.array(StarWarsPerson)
+const StarWarsPeopleResponseData = z.object({
+  results: StarWarsPeopleResults
+})
 
 export const fetchStarWarsPeople = async () => {
-  const data = await fetch(
-    "https://swapi.py4e.com/api/people",
-  ).then((res) => res.json());
+  const data = await fetch('https://swapi.py4e.com/api/people').then((res) =>
+    res.json(),
+  );
 
-  const parsedData = StarWarsPeopleResults.parse(data);
+  const parsedData = StarWarsPeopleResponseData.parse(data);
 
   return parsedData.results;
 };
 
 // TESTS
 
-it("Should return the name", async () => {
+it('Should return the name', async () => {
   expect((await fetchStarWarsPeople())[0]).toEqual({
-    name: "Luke Skywalker",
+    name: 'Luke Skywalker',
   });
 });
